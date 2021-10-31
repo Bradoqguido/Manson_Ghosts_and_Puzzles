bcf04dff74dfa5168204adc470ba2381ac7a1d22
thrivelatti <thrivelatti@gmail.com>
2021-10-31 13:27:26 -0300

    Telas de interacao

---
+++ Scenes/GameOver/GameOver.gd
@@ -0,0 +1,25 @@
+extends CanvasLayer
+
+onready var scene_changer = SceneChanger
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_novo_pressed():
+	scene_changer.change_scene("res://Scenes/1_Porao/Porao.tscn")
+
+
+func _on_sair_pressed():
+	get_tree().quit()
