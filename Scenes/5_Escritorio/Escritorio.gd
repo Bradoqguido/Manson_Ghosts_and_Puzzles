4e459e629f460ad4e1cf249d66eff973ee3f19ad
Jeferson Eduardo Guido <bradoqguido@gmail.com>
2021-10-31 16:35:14 -0300

    Ajustada hitbox da porta do escritório

--- Scenes/5_Escritorio/Escritorio.gd
+++ Scenes/5_Escritorio/Escritorio.gd
@@ -12,8 +12,9 @@ func _ready():
 	pass # Replace with function body.
 
 func _on_Area2D_body_entered(body):
-	AudioHandler.play_sound_door()
-	scene_changer.change_scene("res://Scenes/3_SalaDaMansao/SalaDaMansao.tscn")
+	if body == player:
+		AudioHandler.play_sound_door()
+		scene_changer.change_scene("res://Scenes/3_SalaDaMansao/SalaDaMansao.tscn")
 
 func _input(delta):
 	if get_node_or_null('DialogNode') == null:
