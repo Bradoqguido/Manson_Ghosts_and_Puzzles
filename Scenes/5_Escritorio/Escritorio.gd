3eca6a16a852376db53ec9ad331bc7739bb0e44b
thrivelatti <thrivelatti@gmail.com>
2021-10-31 16:00:54 -0300

    Cofre do escritorio

--- Scenes/5_Escritorio/Escritorio.gd
+++ Scenes/5_Escritorio/Escritorio.gd
@@ -3,11 +3,9 @@ extends Node2D
 onready var scene_changer = SceneChanger
 onready var player = Global.Player
 
-varDialogoChaveAtivo = false
-varChavePrincipalNoInventario = false
-
-varDialogoCofreAtivo = false
-varSenhaCofreNoInventario = false
+var dialogoChaveAtivo = false
+var chavePrincipalNoInventario = false
+var dialogoCofreAtivo = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,3 +13,23 @@ func _ready():
 
 func _on_Area2D_body_entered(body):
 	scene_changer.change_scene("res://Scenes/3_SalaDaMansao/SalaDaMansao.tscn")
+
+func _input(delta):
+	if get_node_or_null('DialogNode') == null:
+		if dialogoCofreAtivo:
+			if Global.senhaDoCofreNoInventario:
+				Global.chamarDialogo("CofreAberto")
+				Global.chaveDaPortaPrincipalDaMansaoNoInventario = true;
+				dialogoCofreAtivo = false
+			else:
+				Global.chamarDialogo("CofreTrancado")
+				dialogoCofreAtivo = false
+
+
+func _on_cofre_body_entered(body):
+	if body == player:
+		dialogoCofreAtivo = true
+
+func _on_cofre_body_exited(body):
+	if body == player:
+		dialogoCofreAtivo = false
