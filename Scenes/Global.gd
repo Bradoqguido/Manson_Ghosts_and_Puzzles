448fb2581daef8ff8feb1f268e46f53d6ac99e91
thrivelatti <thrivelatti@gmail.com>
2021-10-31 16:57:39 -0300

    Interações do quarto

--- Scenes/Global.gd
+++ Scenes/Global.gd
@@ -5,6 +5,7 @@ onready var midNight = false
 onready var chaveDaPortaPrincipalDaMansaoNoInventario = false
 onready var peDeCabraNoInventario = false
 onready var senhaDoCofreNoInventario = false
+onready var chaveDoEscritorioNoInventario = false
 
 func chamarDialogo(dialogo): # Passar o nome do dialogo que você quer executar.
 	get_tree().paused = true # Não esquecer de colocar essa linha na função "on_<objeto>_body_exited" com o valor "false" para despausar o game.
