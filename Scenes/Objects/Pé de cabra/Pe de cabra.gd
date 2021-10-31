dac02aa53228c13c3c327a2df0a09a299ee0d984
Jeferson Eduardo Guido <bradoqguido@gmail.com>
2021-10-31 15:30:36 -0300

    Alterada interação com o pé de cabra

--- Scenes/Objects/Pé de cabra/Pe de cabra.gd
+++ Scenes/Objects/Pé de cabra/Pe de cabra.gd
@@ -3,11 +3,23 @@ extends KinematicBody2D
 onready var player = Global.Player
 onready var peDeCabraNoInventario = Global.peDeCabraNoInventario
 
+var dialogoAtivo = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+func _input(delta):
+	if get_node_or_null('DialogNode') == null:
+		if dialogoAtivo:
+			peDeCabraNoInventario = true
+			Global.chamarDialogo("ChaveDoPorao")
+			$CollisionShape2D.hide()
+			dialogoAtivo = false
+
 func _on_PegarPeDeCabra_body_entered(body):
 	if body == player:
-		peDeCabraNoInventario = true
-		$CollisionShape2D.hide()
+		dialogoAtivo = true
+
+func _on_PegarPeDeCabra_body_exited(body):
+	if body == player:
+		dialogoAtivo = false
